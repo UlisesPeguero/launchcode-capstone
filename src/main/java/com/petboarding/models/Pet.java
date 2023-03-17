455b2f15a40ac447b03fcac5fcde2db7ec8693fc
Ulises Peguero <peguero.ulises@gmail.com>
2023-03-16 22:59:54 -0500

    Remove uneccesary import from models/pet.java

--- src/main/java/com/petboarding/models/Pet.java
+++ src/main/java/com/petboarding/models/Pet.java
@@ -2,7 +2,6 @@ package com.petboarding.models;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "pets")
